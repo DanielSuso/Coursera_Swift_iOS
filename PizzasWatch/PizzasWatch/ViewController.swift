51b1529f480d42a0ac914a98da4c317a620c773a
Mutlicanal3 <Mutlicanal3@minidemlicanal3.iberia.grupotecnocom>
2016-08-04 13:13:57 +0200

    Creación del proyecto

---
+++ PizzasWatch/PizzasWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzasWatch
+//
+//  Created by Mutlicanal3 on 4/8/16.
+//  Copyright © 2016 Daniel Suso Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
